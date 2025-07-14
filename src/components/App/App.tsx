cb564b027f8e96e9d2cdb925fc1f5040f1f6f293
voueva <voueva.e.v@gmail.com>
2025-07-14 20:58:54 +0200

    componets

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,19 @@
-
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
+import Loader from '../Loader/Loader'
+import MovieGrid from '../MovieGrid/MovieGrid'
+import MovieModal from '../MovieModal/MovieModal'
+import SearchBar from '../SearchBar/SearchBar'
 import './App.module.css'
 
 function App() {
 
   return (
     <>
-      
+      <SearchBar></SearchBar>
+      <MovieGrid></MovieGrid>
+      <Loader></Loader>
+      <ErrorMessage></ErrorMessage>
+      <MovieModal></MovieModal>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './components/App/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
