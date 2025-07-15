aa5214964f94a1f5ae456f8c2f962d7b41923d2b
voueva <voueva.e.v@gmail.com>
2025-07-15 18:20:09 +0200

    fetchMovies without BackEnd

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,36 +1,41 @@
 import type { Movie } from "../types/movie";
 
 export function fetchMovies(query: string): Promise<Array<Movie>> {
-
     console.log(query); // TODO: HTTP
-
-    // throw 'Error';
     
-    return Promise.resolve([
-        {
-          id: 5,
-          poster_path: 'dsas',
-          backdrop_path: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
-          title: 'Hello World',
-          overview: 'bh jhb dsbjjh sdbsbj sb sbs dbs dbh sdbjhb sdjbds b bsdb sdb sbj sdbh bsdhb bds bjhs',
-          vote_average: 7,
-          release_date: '17/07/1986'
-        } as Movie,
-        {
-          id: 10,
-          poster_path: 'dsas',
-          backdrop_path: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
-          title: 'Hello World2',
-          overview: 'dcsd',
-          vote_average: 7
-        } as Movie,
-        {
-          id: 11,
-          poster_path: 'dsas',
-          backdrop_path: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
-          title: 'Hello World3',
-          overview: 'dcsd',
-          vote_average: 7
-        } as Movie,
-    ]);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (true) {
+                resolve([
+                    {
+                        id: 5,
+                        poster_path: 'dsas',
+                        backdrop_path: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
+                        title: 'Hello World',
+                        overview: 'bh jhb dsbjjh sdbsbj sb sbs dbs dbh sdbjhb sdjbds b bsdb sdb sbj sdbh bsdhb bds bjhs',
+                        vote_average: 7,
+                        release_date: '17/07/1986'
+                    } as Movie,
+                    {
+                        id: 10,
+                        poster_path: 'dsas',
+                        backdrop_path: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
+                        title: 'Hello World2',
+                        overview: 'dcsd',
+                        vote_average: 7
+                    } as Movie,
+                    {
+                        id: 11,
+                        poster_path: 'dsas',
+                        backdrop_path: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
+                        title: 'Hello World3',
+                        overview: 'dcsd',
+                        vote_average: 7
+                    } as Movie,
+                ]);
+            } else {
+                reject();
+            }
+        }, 2000);
+    });
 }
