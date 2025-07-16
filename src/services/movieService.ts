e14e8c0effd84b0d08a4354568f8d32f27ac8f6a
voueva <voueva.e.v@gmail.com>
2025-07-16 18:49:09 +0200

    fix

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,14 +2,13 @@ import type { Movie } from "../types/movie";
 import axios from 'axios';
 
 const API_URL = 'https://api.themoviedb.org/3';
-const BEARER_TOKEN = import.meta.env.VITE_API_KEY;
 
 export const fetchMovies = async (query: string): Promise<Array<Movie> | undefined> => {
     try {
       const response = await axios.get(`${API_URL}/search/movie`, {
         params: { query },
         headers: {
-          Authorization: `Bearer ${BEARER_TOKEN}`,
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
           accept: 'application/json'
         }
       });
